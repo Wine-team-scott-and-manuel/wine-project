4f85b4d6cc8b750a1ac2e2a498d22e6379dbe792
Scott Barnett <scottbarnett97@gmail.com>
2023-05-23 16:55:26 -0500

    updates from day 1

---
+++ functions.py
@@ -0,0 +1,83 @@
+######IMPORTS#####
+
+# Ignore Warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+# Basics:
+import pandas as pd
+import numpy as np
+import math
+import numpy as np
+import scipy.stats as stats
+import os
+
+# Data viz:
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# Sklearn stuff:
+import sklearn
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import mutual_info_score
+
+from sklearn.model_selection import train_test_split
+
+## Regression Models
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import LinearRegression, LassoLars, TweedieRegressor
+from sklearn.preprocessing import PolynomialFeatures
+
+## Classification Models
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.ensemble import GradientBoostingClassifier
+
+from sklearn.metrics import classification_report, confusion_matrix, plot_confusion_matrix
+
+## local
+import wrangle
+
+
+
+###### VISUALIZATIONS ########
+
+
+
+
+
+
+
+###### STATS ########
+
+def run_lattest(data):
+    '''
+    runs a Ttest for LA county vs tax value
+    '''
+    x = data['county_LA']
+    y = data['taxvalue']
+    # Perform t-test
+    t_statistic, p_value = stats.ttest_ind(x, y)
+    # Decide whether to reject the null hypothesis
+    alpha = 0.05
+    if p_value == alpha:
+        decision = "Fail to Reject Null Hypothesis"
+    else:
+        decision = "Reject Null Hypothesis"
+# Create a DataFrame to store the results
+    results = pd.DataFrame({
+        'T-Statistic': [t_statistic],
+        'P-Value': [p_value],
+        'Decision': [decision]})
+    return results
+
+
+
+
+
--- wrangle.py
+++ wrangle.py
@@ -2,22 +2,47 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-import env
-import os
- 
+from sklearn.model_selection import train_test_split
 
 ####################### Imports ############################
 
-def check_file_exists(fn, query, url):
+def get_wine():
     '''
-    this function checks to see if the .csv file already exists. If yes it reads it
+    combined csv needs to be name 'combined_wine.csv' in local drive
+    renames columns for redability
+    converts mg/L columns to g/L for uniformity
+    creates dummy for wine_type
+    returns df
     '''
-    if os.path.isfile(fn):
-        print('csv file found and loaded\n')
-        return pd.read_csv(fn, index_col=0)
-    else: 
-        print('first create combined_wine.csv')
-        return df 
+    df = pd.read_csv('combined_wine.csv')
+    new_col_name = []
+    for col in df.columns:
+        new_col_name.append(col.lower().replace(' ', '_'))
+    df.columns = new_col_name
+    df['total_sulfur_dioxide'] = df.total_sulfur_dioxide / 1000
+    df['free_sulfur_dioxide'] = df.free_sulfur_dioxide / 1000
+    dummy_df = pd.get_dummies(df['wine_type'], drop_first=True)
+    df = pd.concat([df, dummy_df], axis=1)
+    return df
+    
+def split_data(df):
+    '''
+    Be sure to code it as train, validate, test = split_data(df)
+    take in a DataFrame and return train, validate, and test DataFrames; .
+    return train, validate, test DataFrames.
+    '''
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_validate, 
+                                       test_size=.25, 
+                                       random_state=123, 
+                                       )
+    #This confirms and Validates my split.
+    
+    print(f'train -> {train.shape}, {round(train.shape[0]*100 / df.shape[0],2)}%')
+    print(f'validate -> {validate.shape},{round(validate.shape[0]*100 / df.shape[0],2)}%')
+    print(f'test -> {test.shape}, {round(test.shape[0]*100 / df.shape[0],2)}%')
+    
+    return train, validate, test  
     
 
     
